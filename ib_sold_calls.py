d3e717e13fa6a3281eb668070c673c1e06e75604
kalmukdmitri <kalmukdmitri@gmail.com>
2022-02-16 19:25:22 +0300

    UA exra log

--- ib_sold_calls.py
+++ ib_sold_calls.py
@@ -6,6 +6,7 @@ import pandas
 from oauth2client.service_account import ServiceAccountCredentials
 from google.oauth2 import service_account
 import gspread
+import base64
 
 key_path = '/home/web_analytics/m2-main-cd9ed0b4e222.json'
 key_path_extra = '/home/web_analytics/other_keys.json'
@@ -22,8 +23,8 @@ sh = gc.open_by_key("1xMDWCSt6Br5kCp1ekLGlIFLAGrHvWCaljgnXBngUieI")
 wk = sh.worksheet('list')
 list_of_dicts = wk.get_all_records()
 IB_CALLS = pandas.DataFrame(list_of_dicts)
-
-
+IB_CALLS = IB_CALLS[IB_CALLS['Parsable'] == 'TRUE']
+IB_CALLS = IB_CALLS.drop(columns = ['Parsable'])
 res = []
 for i in IB_CALLS.itertuples():
     row = list(i[1:])
@@ -46,7 +47,17 @@ def sums_cleans(MONEYS):
     if type(MONEYS) == str:
         MONEYS = 0
     return MONEYS
+
 ib_result['Sum_mort'] = ib_result['Sum_mort'].apply(sums_cleans)
 ib_result['Gain'] = ib_result['Gain'].apply(sums_cleans)
+
+def b64_hid(s):
+
+    b = s.encode("UTF-8")
+    e = base64.b64encode(b)
+    s1 = e.decode("UTF-8")
+    return s1
+ib_result['Client'] = ib_result['Client'].apply(b64_hid)
+
 
 ib_result.to_gbq(f'GOOGLE_SHEETS_DATA.IB_FINISED', project_id='m2-main', if_exists='replace', credentials=gbq_credential)
