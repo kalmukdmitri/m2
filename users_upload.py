738843260d5f15fec43655d87d72b8ed07d0ec64
kalmukdmitri <kalmukdmitri@gmail.com>
2022-06-30 13:23:08 +0300

    postbacks

--- users_upload.py
+++ users_upload.py
@@ -30,29 +30,30 @@ def date_pairs(date1, date2, step= 1):
 #         s2 = urllib.parse.unquote(s2)
 #     return s2
 
-def unix_preprocessing(date):
-    if ":" in date:
-        text = date.lower()
-        text = re.sub('[a-zA-Z]+', ' ', text)
-        text = re.sub('\+[0-9]+', ' ', text)
-        text = re.sub('\.[0-9]+', ' ', text)
-        text = re.sub('\ :[0-9]+', ' ', text)
-        text = re.sub('\ -[0-9]+', ' ', text)
-        text = re.sub('\ :[0-9]+', '', text)
-        return text.strip()        
-    else:
-        unix_timestamp = float(date)
-        a = (datetime.datetime.utcfromtimestamp(unix_timestamp).strftime('%Y-%m-%d %H:%M:%S'))
-    return a 
+# def unix_preprocessing(date):
+#     if ":" in date:
+#         text = date.lower()
+#         text = re.sub('[a-zA-Z]+', ' ', text)
+#         text = re.sub('\+[0-9]+', ' ', text)
+#         text = re.sub('\.[0-9]+', ' ', text)
+#         text = re.sub('\ :[0-9]+', ' ', text)
+#         text = re.sub('\ -[0-9]+', ' ', text)
+#         text = re.sub('\ :[0-9]+', '', text)
+#         return text.strip()        
+#     else:
+#         unix_timestamp = float(date)
+#         a = (datetime.datetime.utcfromtimestamp(unix_timestamp).strftime('%Y-%m-%d %H:%M:%S'))
+#     return a 
 
 # key_path = 'C:\\Users\\kalmukds\\NOTEBOOKs\\projects\\keys\\m2-main-cd9ed0b4e222.json'
+
 key_path = '/home/web_analytics/m2-main-cd9ed0b4e222.json'
+
 gbq_credential = service_account.Credentials.from_service_account_file(key_path,)
-q = """SELECT  MAX(dateHourMinute) as date FROM `m2-main.ames.NB_UP_sources` """
+
+q = """SELECT MAX(dateHourMinute) as date FROM `m2-main.UA_REPORTS.USERS_DT`"""
 last_dt = pandas_gbq.read_gbq(q, project_id='m2-main', credentials=gbq_credential)
 start = last_dt['date'][0].date() + datetime.timedelta(days=1)
-
-
 
 ga_conc = ga_connect('208464364')
 tries = 200
@@ -62,28 +63,24 @@ while start < datetime.datetime.today().date():
     
     try:
         dates_couples = date_pairs(start,start)
-
+        
         print(dates_couples)
-
-
+        
         params =  {'dimetions': [
                                  {'name': 'ga:dateHourMinute'},
-                                 {'name': 'ga:dimension5'}, #hittimestamp
-                                 {'name': 'ga:previousPagePath'},
-                                 {'name': 'ga:pagePath'},
-                                 {'name': 'ga:dimension4'}
+                                 {'name': 'ga:dimension1'},
+                                 {'name': 'ga:dimension2'},
+                                 {'name': 'ga:dimension3'}
                                  ],
                     'metrics':   [
                                  {'expression': 'ga:users'}
-                                 ],
-                    'filters': 'ga:previousPagePath=~/nedvizhimost;ga:pagePath=~/novostr'}
-
+                                 ]}
         all_traf_new = ga_conc.report_pd(dates_couples,params)
+                   
         all_traf_new['dateHourMinute'] = all_traf_new['dateHourMinute'].apply(lambda x: datetime.datetime.strptime(x,"%Y%m%d%H%M"))
 
-        all_traf_new['dimension5'] = [unix_preprocessing(words) for words in all_traf_new['dimension5']]
-
-        all_traf_new.to_gbq(f'ames.NB_UP_sources', project_id='m2-main',chunksize=20000, if_exists='append', credentials=gbq_credential)
+        all_traf_new.to_gbq(f'UA_REPORTS.USERS_DT', project_id='m2-main',chunksize=20000, if_exists='append', credentials=gbq_credential)
+        
         start += datetime.timedelta(days=1)
         time.sleep(10)
     except:
