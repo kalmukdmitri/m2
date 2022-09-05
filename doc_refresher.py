2ef99ea4f8a005fea9565f75af693d86ff2565b1
kalmukdmitri <kalmukdmitri@gmail.com>
2022-09-05 20:57:33 +0300

    GA_FULL

--- doc_refresher.py
+++ doc_refresher.py
@@ -56,7 +56,9 @@ def get_tabel_OPG(month):
     return full_res
 
 res_list = []
-month_list = ["Август"]
+month_list = [
+#     "Август",
+    "Сентябрь"]
 for i in month_list:
     res_list.append(get_tabel_OPG(i))
 final = pandas.concat(res_list)
@@ -73,9 +75,9 @@ def sheet_ready(df_r):
     return rows
 
 sh = gc.open_by_key("17p9apeWzq_Cg8HnyD25XT-UiHT5-xSI-94bzDjMVooY")
-wk = sh.worksheet('MSK 2022.08')
+wk = sh.worksheet('MSK 2022.09')
 g_clop=sheet_ready(final_m)
 wk.update('A1',g_clop)
-wk = sh.worksheet('СПБ 2022.08')
+wk = sh.worksheet('СПБ 2022.09')
 g_clop=sheet_ready(final_s)
 wk.update('A1',g_clop)
